761c28e9e5e7bd836152ca19d1ea2c8c3a4dcdbd
zaza <91153901+penosbol@users.noreply.github.com>
2026-04-10 16:21:37 +0100

    jsedit

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const cover = document.querySelector('.cover');
 const btn = document.getElementById('Open');
-const firstPage = document.querySelector('.page');
 
 btn.addEventListener('click', () => {
-
     cover.classList.toggle('open');
-    firstPage.classList.toggle('visible');
     btn.textContent = cover.classList.contains('open') ? 'Close' : 'Open';
-
-}
-)
-    ;
+})
+;
