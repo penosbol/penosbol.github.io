e3db45418319a5d1f927366b2d0bb76a2e10e49b
zaza <91153901+penosbol@users.noreply.github.com>
2026-04-10 17:26:42 +0100

    debuggggg

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const btn = document.getElementById('Open');
 let state = 0;
 
 btn.addEventListener('click', () => {
-  if (state === 0) {
-    cover.classList.add('open');
-    page2.style.display = 'block';
-    state = 1;
-  }
-
-}
-)
-
+    if (state === 0) {
+        cover.classList.add('open');
+        page2.style.display = 'block';
+        btn.textContent = 'Close';
+        state = 1;
+    } else if (state === 1) {
+        cover.classList.remove('open');
+        page2.style.display = 'none';
+        btn.textContent = 'Open';
+        state = 0;
+    }
+    }
+    )
 ;
